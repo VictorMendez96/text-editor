3a057e720b2d21d42783f4f050ec1a42d9a12ff6
V Mendez <victormendez990@gmail.com>
2022-10-23 23:04:23 -0600

    PUT logic for DB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,11 +15,15 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readwrite')
+  const tx = db.transaction('jate', 'readwrite');
 
-  const store = tx.objectStore('jate')
+  const store = tx.objectStore('jate');
 
-  const request = store.add({})
+  const request = store.put({ jate: content });
+
+  const result = await request;
+
+  return result;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
